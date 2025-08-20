baf4ac5cbbdf6113bf44c85bf3981beae5d8f87a
Saad Mohammad Shibli <saadshibli27@gmail.com>
2025-08-20 08:57:38 +0530

    Add Q.1920 Build Array From Permutation

---
+++ 1920.buildArrayFromPermutation.cpp
@@ -0,0 +1,11 @@
+// Problem : 1920. Build Array From Permutation
+// Source : https://leetcode.com/problems/build-array-from-permutation/
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0; i<nums.size(); i++) ans.push_back(nums[nums[i]]);
+        return ans;
+    }
+};
