958c7d87de2b83890d903ad3312240c2be46deb6
Saad Mohammad Shibli <saadshibli27@gmail.com>
2025-08-21 11:01:46 +0530

    Add Q.2769 Find the Maximum Achievable Number

---
+++ 2769.maxAchievableNum.cpp
@@ -0,0 +1,9 @@
+// Problem : 2769. Find the Maximum Achievable Number
+// Source : https://leetcode.com/problems/find-the-maximum-achievable-number/description/
+
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        return num+2*t;
+    }
+};
