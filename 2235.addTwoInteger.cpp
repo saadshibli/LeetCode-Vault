76d38cddd5b3dadf64b5150e54156ad66e0975ed
Saad Mohammad Shibli <saadshibli27@gmail.com>
2025-08-24 15:57:55 +0530

    Add Q.2235 Add Two Integer

---
+++ 2235.addTwoInteger.cpp
@@ -0,0 +1,9 @@
+// Problem : Add Two Integers
+// Source : https://leetcode.com/problems/add-two-integers/description/
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1+num2;
+    }
+};
