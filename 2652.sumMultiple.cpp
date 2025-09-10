a7d9e9b822a9d1de3b19e4e5ef7cf2dfb5d85def
Saad Mohammad Shibli <saadshibli27@gmail.com>
2025-09-10 13:20:45 +0530

    Add Q.2652 Sum Multiples

---
+++ 2652.sumMultiple.cpp
@@ -0,0 +1,13 @@
+// Problem : 2652. Sum Multiples
+// Source : https://leetcode.com/problems/sum-multiples/
+
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        int sum=0;
+        for(int i=1; i<=n; i++) {
+            if(i%3==0||i%5==0||i%7==0) sum+=i;
+        }
+        return sum;
+    }
+};
