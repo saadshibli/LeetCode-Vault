c6fea30c33142cec4656e20232cde26c8acd10d2
Saad Mohammad Shibli <saadshibli27@gmail.com>
2025-10-29 02:37:01 +0530

    Add Q.1512 Number of Good Pairs

---
+++ 1512.noOfGoodPairs.cpp
@@ -0,0 +1,15 @@
+// Problem : 1512. Number of Good Pairs
+// Source : https://leetcode.com/problems/number-of-good-pairs/
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int pair = 0;
+        for(int i=0; i<nums.size(); i++) {
+            for(int j=i+1; j<nums.size(); j++) {
+                if(nums[i]==nums[j] && i<j) pair++;
+            }
+        }
+        return pair;
+    }
+};
