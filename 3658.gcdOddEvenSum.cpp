572c5f949655b0ba5f9f6d4c9371f976be1ac68c
Saad Mohammad Shibli <saadshibli27@gmail.com>
2025-11-07 15:24:22 +0530

    Add Q.3658 GCD of Odd and Even Sums

---
+++ 3658.gcdOddEvenSum.cpp
@@ -0,0 +1,9 @@
+// Problem : 3658. GCD of Odd and Even Sums
+// Source : https://leetcode.com/problems/gcd-of-odd-and-even-sums/
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
